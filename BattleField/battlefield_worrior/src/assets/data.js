07e44f7931a144cb743eab8cb7ad9215a87dd12b
Thomas <codyhh.91@gmail.com>
2026-01-08 00:55:39 +0700

    COC Style Testing and first commit

---
+++ BattleField/battlefield_worrior/src/assets/data.js
@@ -0,0 +1,23 @@
+export const worriors = [
+  {
+    id: 1,
+    name: "Babarian",
+    power: 1000,
+    price: 20,
+    img: "barbar.jpg",
+  },
+  {
+    id: 2,
+    name: "Archar",
+    power: 900,
+    price: 25,
+    img: "arar.jpg",
+  },
+  {
+    id: 3,
+    name: "Dragon",
+    power: 2000,
+    price: 40,
+    img: "drdr.jpg",
+  },
+];
